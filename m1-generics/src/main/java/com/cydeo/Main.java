3dbd63f830c14088cf7abe24a5322a5afd9585cc
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-15 13:16:09 +0200

    generics

--- m1-generics/src/main/java/com/cydeo/Main.java
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -21,6 +21,34 @@ public class Main { // <Class Name> generics
 
         printDoubled(items);
 
+
+        System.out.println("----------------------------------------------------");
+
+        FootballPLayer joe = new FootballPLayer("joe"); // joe registering himself to football player
+        BaseballPlayer pat = new BaseballPlayer("pat");
+        SoccerPlayer beckham = new SoccerPlayer("beckham");
+
+        Team<SoccerPlayer> liverpool = new Team("liverpool"); // Team accepting any object
+        //liverpool.addPlayer(joe);
+        //liverpool.addPlayer(pat);
+        liverpool.addPlayer(beckham);
+
+        // but problem is liverpool is a soccer team, it should only accept soccer player, not baseball-football players
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
     private static void printDoubled(ArrayList<Integer> items) {
