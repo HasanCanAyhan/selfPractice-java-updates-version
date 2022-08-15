8eede09c997de857e45c5218cbc79f42a6fbc3c5
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-15 14:26:27 +0200

    enum

--- m2-enum/src/main/java/com/cydeo/Operation.java
+++ m2-enum/src/main/java/com/cydeo/Operation.java
@@ -6,7 +6,10 @@ public enum Operation {
 
 
     // we can not put public by creating Constructor
-    private Operation() {  // 4 times executed because each of them is object of Operation
+    private Operation() {
+        // 4 times executed because each of them is object of Operation
+        //Enum can have constructor.
+        // For each enum constant(Object), constructor will be executed.
         System.out.println("Constructor");
     }
 }
