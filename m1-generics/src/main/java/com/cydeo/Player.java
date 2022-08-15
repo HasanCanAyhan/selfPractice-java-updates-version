c6facb823796f90ee49bc0c386091e7de9d4b087
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-15 12:59:07 +0200

    generics

---
+++ m1-generics/src/main/java/com/cydeo/Player.java
@@ -0,0 +1,21 @@
+package com.cydeo;
+
+public abstract class Player {
+
+    // Custom Generics Class
+
+    // different teams
+
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+}
