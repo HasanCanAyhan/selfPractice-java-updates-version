eab8fbf213cc4269838eb7c7917e1682f8b6eae9
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-16 12:14:07 +0200

    lambda

---
+++ m3-lambda/src/main/java/com/cydeo/yt_practice/Main.java
@@ -0,0 +1,28 @@
+package com.cydeo.yt_practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Bro";
+        char symbol = '!';
+
+        /*
+        MyInterface myInterface1 = new MyInterface() {
+            @Override
+            public void message(String name, char symbol) {
+
+            }
+        }
+
+         */
+        MyInterface myInterface = (x,y) -> {
+            System.out.println("Hello World!");
+            System.out.println("It is a nice day " + x +y);
+        };
+        myInterface.message(name,symbol);
+
+
+    }
+
+}
---
+++ m3-lambda/src/main/java/com/cydeo/yt_practice/MyInterface.java
@@ -0,0 +1,12 @@
+package com.cydeo.yt_practice;
+
+//https://youtu.be/LEJ1kGHSXdA
+
+@FunctionalInterface
+public interface MyInterface {
+
+    // one abstract method
+
+    public void message(String name, char symbol);
+
+}
