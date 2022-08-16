1872ed4f4a71637afb29b358c4f9186c4b3ea12e
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-16 15:01:53 +0200

    lambda

--- m3-lambda/src/main/java/com/cydeo/behaviorParameterization/Main.java
+++ m3-lambda/src/main/java/com/cydeo/behaviorParameterization/Main.java
@@ -7,6 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Behavior Parameterization : the ability to tell a method to take
+        //multiple behaviors (strategies)
+
         // interface
         // class which implement interface
         // method-action
@@ -14,31 +17,45 @@ public class Main {
 
         List<Apple> inventory = new ArrayList<Apple>();
 
-        inventory.add(new Apple(300,Color.GREEN));
-        inventory.add(new Apple(100,Color.RED));
-        inventory.add(new Apple(200,Color.GREEN));
-        inventory.add(new Apple(50,Color.RED));
+        inventory.add(new Apple(300, Color.GREEN));
+        inventory.add(new Apple(100, Color.RED));
+        inventory.add(new Apple(200, Color.GREEN));
+        inventory.add(new Apple(50, Color.RED));
 
 
         HeavyApplePredicate heavyApplePredicate = new HeavyApplePredicate();
         GreenApplePredicate greenApplePredicate = new GreenApplePredicate();
 
+        filterApple(inventory, heavyApplePredicate);
+        filterApple(inventory, greenApplePredicate);
 
-       filterApple(inventory,heavyApplePredicate);
-       filterApple(inventory,greenApplePredicate);
 
+        // or
+
+        /*
+        // Achtung! : return type of method should be List<Apple>
+        ApplePredicate abc = new AppleHeavyPredicate();
+        List<Apple> heavyApple = filterApples(inventory,abc);
+        System.out.println(heavyApple);
+
+        List<Apple> greenApple = filterApples(inventory,new AppleGreenColorPredicate());
+        System.out.println(greenApple);
+         */
 
 
     }
 
-    private static void filterApple( List<Apple> inventory, ApplePredicate applePredicate){
+
+    //                  this method : get the all apples , based on the action give me result
+    private static void filterApple(List<Apple> inventory, ApplePredicate applePredicate) {
         //                                                   // interface in order to take different actions
+        //                                                   //  heavyApple behaviors or greenApple behaviors
 
         List<Apple> result = new ArrayList<>();
 
         for (Apple apple : inventory) {
 
-            if (applePredicate.test(apple)){
+            if (applePredicate.test(apple)) {
                 result.add(apple);
             }
 
