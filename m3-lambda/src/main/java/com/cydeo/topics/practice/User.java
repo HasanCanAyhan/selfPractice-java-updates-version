29a96e8899cc6788080145755ecfb456b16573ed
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-17 23:24:23 +0200

    lambda

---
+++ m3-lambda/src/main/java/com/cydeo/topics/practice/User.java
@@ -0,0 +1,18 @@
+package com.cydeo.topics.practice;
+
+
+import lombok.*;
+
+@Data // @Getter @Setter @ToString
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+}
