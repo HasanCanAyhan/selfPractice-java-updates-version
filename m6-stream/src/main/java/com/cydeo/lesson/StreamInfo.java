b523013af3f929d2d4f1a869b2f003e47f7cc428
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-19 16:06:54 +0200

    stream

---
+++ m6-stream/src/main/java/com/cydeo/lesson/StreamInfo.java
@@ -0,0 +1,28 @@
+package com.cydeo.lesson;
+
+public class StreamInfo {
+
+    /*
+
+    Stream - Flow - supports data processing operations
+    Stream Pipeline is the operations that run on a stream to produce a result.
+
+    we use  lambda expreession i the Stream
+
+    Stream Source : an Array, collection , I/O channel , ArrayList
+    first, we  need to create Stream , briefly we put collection in to Stream, convert it to Stream
+    then, inside the stream I can implementing certain operations for each data like filter, sorting, mapping etc..
+    there are different actions
+    next one, Terminal Operation: after completed operations, we need to take collection from Stream, from the flow, otherweise, it will be inside the forever circle
+    exit the collection to outside and use it.
+
+
+
+
+
+
+
+
+     */
+
+}
