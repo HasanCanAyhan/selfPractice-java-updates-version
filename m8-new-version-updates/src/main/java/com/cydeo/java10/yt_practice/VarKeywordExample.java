574ed0f8ee1753d9a69cca3540ba20eb976e98b3
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-24 00:29:29 +0200

    new java versions

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java10/yt_practice/VarKeywordExample.java
@@ -0,0 +1,47 @@
+package com.cydeo.java10.yt_practice;
+
+import java.util.ArrayList;
+
+public class VarKeywordExample {
+
+    // var i = 10; --->>> you can not use "var" as instance variable
+
+    public static void main(String[] args) {
+
+        //Local-Variable Type Inference
+
+        /*
+        It allows to define a variable using var and
+        without specifying the type of it. The compiler infers the type of the variable
+        using the value provided. This type inference is restricted to local variables.
+         */
+
+        //int i = 10;
+        var n = 10;
+
+        var str = "Hello world";
+
+        var names = new ArrayList<String>();
+        names.add("10");
+
+        var first = "Can";
+        var last = "Ay";
+        var fullName = first + last;
+        System.out.println(fullName);
+
+        //var myLambda =  () -> System.out.println("Hello"); --->>> you can not use!
+
+
+
+
+    }
+
+    /* you can not use "var" like this
+    public var int add(var a, var b){
+        return a+b;
+    }
+     */
+
+
+
+}
