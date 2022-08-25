e4e35bb2a1e59e36224951072e984cc264574f71
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-25 22:13:01 +0200

    java new versions

--- m8-new-version-updates/src/main/java/com/cydeo/java11/StringMethodDemo.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java11/StringMethodDemo.java
@@ -5,10 +5,14 @@ public class StringMethodDemo {
     public static void main(String[] args) {
 
         //lines() :
+        //lines(): It is used to get all the lines in a String as a Stream.
 
         var str = "Mike \n Ozzy \n Tom \n Emmy";
 
         str.lines().forEach(System.out::println);
+
+        Long count = str.lines().count();
+        System.out.println("count = " + count);
 
 
 
