79c48b8b237fea4440d61ea54c1af699ef97fa86
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-25 22:36:22 +0200

    java new versions

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,14 @@
+package com.cydeo.java14;
+
+public record User(String name, String address) {
+
+    //private final
+
+
+    // All ARG CONST
+    // GETTER
+    // TO STRING
+    // EQUALS
+    // HASH CODE
+
+}
