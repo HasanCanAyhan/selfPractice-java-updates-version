ad85c49e7b8dc43c44b5a7b433dd0a6064a500c3
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-25 21:51:56 +0200

    java new versions

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java10/OrElseThrowDemo.java
@@ -0,0 +1,27 @@
+package com.cydeo.java10;
+
+import java.util.Optional;
+
+public class OrElseThrowDemo {
+
+    public static void main(String[] args) {
+
+        // isPresent + get
+
+        Optional<String> str = Optional.empty();
+
+        //System.out.println(str.get());
+
+        if (str.isPresent()){
+            System.out.println(str.get());
+        }else{
+            // exception handling
+        }
+
+        System.out.println(str.orElseThrow());
+        System.out.println(str.orElseThrow(() -> new RuntimeException()));
+
+
+    }
+
+}
