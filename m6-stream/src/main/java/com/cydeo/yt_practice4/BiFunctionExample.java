a9c8f5233f3614f6144504ef1166e10d42e0c4b9
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-08-26 14:18:25 +0200

    stream

---
+++ m6-stream/src/main/java/com/cydeo/yt_practice4/BiFunctionExample.java
@@ -0,0 +1,37 @@
+package com.cydeo.yt_practice4;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+//https://youtu.be/D5aS3otGjWg
+public class BiFunctionExample implements BiFunction<Integer,Integer,Integer>{
+
+    public static void main(String[] args) {
+
+        // BiFunction takes 2 argument returns any object : R apply(T t, U u);
+
+        BiFunction<Integer,Integer,Integer> biFunction = new BiFunctionExample();
+        System.out.println(biFunction.apply(10,20));
+
+        System.out.println("---------------------------------------------");
+        //using Lambda
+
+        BiFunction<Integer,Integer,Integer> biFunction2 = (a,b) -> a+b;
+        System.out.println( biFunction2.apply(10,20) );
+
+        //andThen method:
+
+        Function<Integer,Integer> function = (number) -> number*number;
+
+        Integer result = biFunction2.andThen(function).apply(10,20);
+
+        System.out.println(result);
+
+    }
+
+
+    @Override
+    public Integer apply(Integer t, Integer u) {
+        return (t + u );
+    }
+}
